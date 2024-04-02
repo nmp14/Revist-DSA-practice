cb045ba83f5b5c43c8189dc536e37bad3f259248
Nathan Peek <nmp14fsu@gmail.com>
2024-04-01 23:49:00 -0400

    Test perforamnce.now

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 console.log('Hello world');
 const readline = require('node:readline');
+
+
+const timeOne = performance.now();
+const num = function addUpTo(n: number) {
+    let total = 0;
+    for (let i = 0; i <= n; i++) {
+        total += i;
+    }
+    return total;
+} (20000000000);
+const timeTwo = performance.now();
+
+console.log((timeTwo - timeOne) / 1000);
+console.log(num);
+
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
